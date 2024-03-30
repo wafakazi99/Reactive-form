29e4c6960b2e65c2cce7991d74aecf5c4dab756e
wafa kazi <wafakaziofficial99@gmail.com>
2024-03-30 21:38:47 +0530

    made a form page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatTabsModule,MatSelectModule,MatButtonModule,TextFieldModule,MatFormFieldModule,MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
